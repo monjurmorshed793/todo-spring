b035d05cf07f61fdf56cf4084ba4ed62a48dfd39
monjurmorshed793 <monjurmorshed793@gmail.com>
2022-11-28 12:52:57 +0600

    continued work on test containers.

--- src/main/java/com/morshed/todo/user/management/repository/UserRepository.java
+++ src/main/java/com/morshed/todo/user/management/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.morshed.todo.user.management.repository;
 
 import com.morshed.todo.user.management.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.ListPagingAndSortingRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends ListPagingAndSortingRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 }
--- src/test/java/com/morshed/todo/TodoApplicationTests.java
+++ src/test/java/com/morshed/todo/TodoApplicationTests.java
@@ -1,13 +1,33 @@
 package com.morshed.todo;
 
+import com.morshed.todo.user.management.repository.UserRepository;
+import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("testcontainers")
 class TodoApplicationTests {
+
+	@Container
+	public static TodoPostgresqlContainer container = TodoPostgresqlContainer.getInstance();
+
+	@Autowired
+	private UserRepository userRepository;
 
 	@Test
 	void contextLoads() {
+		var countLong = userRepository.count();
 	}
 
 }
